90ee9ca28a9ad8087ed398e669583aa7c0edfff9
aayush287 <aayushchaudhary287@gmail.com>
2024-03-14 10:03:45 +0530

    feat: Added custom component for inline code

--- src/BlogRenderer.js
+++ src/BlogRenderer.js
@@ -26,9 +26,39 @@ const markdown = `# Hello, Markdown!
 ## Quotes
 
 > This is a blockquote.
-> It can span multiple lines.`;
+> It can span multiple lines.
+
+<inlinecode>Inline code</inlinecode>`;
+
 const BlogRenderer = () => {
-  return <Markdown>{markdown}</Markdown>;
+  return (
+    <Markdown
+      options={{
+        overrides: {
+          inlinecode: InlineCode,
+        },
+      }}
+    >
+      {markdown}
+    </Markdown>
+  );
 };
 
 export default BlogRenderer;
+
+const InlineCode = ({ children }) => {
+  return (
+    <span
+      style={{
+        "border-radius": "5px",
+        background: "rgba(238, 237, 237, 0.823)",
+        border: "1px solid #adadad",
+        padding: "0px 6px",
+        display: "inline",
+      }}
+    >
+      {" "}
+      {children}{" "}
+    </span>
+  );
+};
